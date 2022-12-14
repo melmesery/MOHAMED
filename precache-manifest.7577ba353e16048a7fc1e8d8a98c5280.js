1bd70f328195913389fd3e8c48dcee1b57ebb436
Mohamed El Mesery <melmesery@outlook.com>
2022-12-14 21:35:32 +0200

    Updates

---
+++ precache-manifest.7577ba353e16048a7fc1e8d8a98c5280.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "98ee4307274081fcf454",
+    "url": "/MOHAMED/static/css/main.73167a3f.chunk.css"
+  },
+  {
+    "revision": "98ee4307274081fcf454",
+    "url": "/MOHAMED/static/js/main.98ee4307.chunk.js"
+  },
+  {
+    "revision": "571bb959c8611f89dd37",
+    "url": "/MOHAMED/static/css/1.661bb9e5.chunk.css"
+  },
+  {
+    "revision": "571bb959c8611f89dd37",
+    "url": "/MOHAMED/static/js/1.571bb959.chunk.js"
+  },
+  {
+    "revision": "2024334cf82d4062af58",
+    "url": "/MOHAMED/static/js/runtime~main.2024334c.js"
+  },
+  {
+    "revision": "64e05dae6e0ad4439ef4811d72c55266",
+    "url": "/MOHAMED/static/media/profile.64e05dae.jpg"
+  },
+  {
+    "revision": "26fe9d38cbdf399e247333ab085d0e1b",
+    "url": "/MOHAMED/static/media/project_1.26fe9d38.png"
+  },
+  {
+    "revision": "d89ec3a879fd8577878fb1bbf59a57d6",
+    "url": "/MOHAMED/static/media/project_2.d89ec3a8.png"
+  },
+  {
+    "revision": "597046b36bf9e66bfe5d8d1a8a898ab9",
+    "url": "/MOHAMED/static/media/project_3.597046b3.png"
+  },
+  {
+    "revision": "cb1c4021bd6fc26e4c840d31e4addc11",
+    "url": "/MOHAMED/static/media/project_4.cb1c4021.png"
+  },
+  {
+    "revision": "e89c1016f7d1f021b2ebd8fa14706cad",
+    "url": "/MOHAMED/index.html"
+  }
+];
